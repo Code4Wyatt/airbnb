ef7943ff0dd5fed9a8457f8110d6905885d0e6d9
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-04-12 15:46:34 +0100

    Banner Updated
    Working on date picker feature

--- airbnb/src/pages/Home.jsx
+++ airbnb/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './home.scss'
+import Banner from "../components/Banner/Banner"
 
 const Home = () => {
   return (
       <div className="home">
-          <h1>Home</h1>
+      <Banner />
    </div>
   )
 }
