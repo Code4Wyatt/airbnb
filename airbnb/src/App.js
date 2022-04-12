11aed66a61ecf25edea888e7dfb4024feb278751
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-04-12 14:55:06 +0100

    Header Component completed

---
+++ airbnb/src/App.js
@@ -0,0 +1,29 @@
+import './App.css'
+import Home from "../src/pages/Home"
+import Header from "./components/Header/Header"
+
+function App() {
+  return (
+
+    // BEM Naming Conventions
+
+    <div className="app">
+      <Header />
+      <Home />
+    {/* Home */}
+      {/* Header */}
+      
+      {/* Banner */}
+      
+      {/* Cards */}
+
+      {/* Footer */}
+      
+    {/* Search */}
+    
+    </div>
+
+  );
+}
+
+export default App;
